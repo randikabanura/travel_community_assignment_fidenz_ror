66f72a2154b76dca29d95e4643b3d24835004c2d
randikabanura <randika.banura@gmail.com>
2019-09-04 09:57:23 +0530

    Double loading fixed of home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+  User.create({
+      name: Faker::Name.name,
+      email: Faker::Internet.free_email,
+      password: "password",
+      gender: "m",
+      dob: "12/10/1992"
+              })
+end
