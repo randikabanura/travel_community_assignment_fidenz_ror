a8a9490253d2c10d96c185d313a824188485be8c
randikabanura <randika.banura@gmail.com>
2019-09-06 23:59:59 +0530

    Trip upload function works with images

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,5 +1,6 @@
 class TripsController <ApplicationController
 
+  before_action :trip_params, only: [:create]
   def index
 
   end
@@ -9,14 +10,20 @@ class TripsController <ApplicationController
   end
 
   def create
-    #render plain: params[:trip].inspect
     @trip = Trip.new(trip_params)
-    @trip.save
+    @trip.user = current_user if current_user
+    if @trip.save
+      flash.now[:notice] = "Assignment was saved successfully."
+      render :new
+    else
+      flash.now[:alert] = "Error creating assignment. Please make sure there is a name and description."
+      render :new
+    end
   end
 
   private
 
   def trip_params
-    params.require(:trip).permit(:location, :date_s, :date_e, :description, :photos)
+    params.require(:trip).permit( :location, :date_s, :date_e, :description, :photos => [])
   end
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,20 @@
 class Trip < ApplicationRecord
   validates :location, presence: true
+  validate :image_type
 
-  has_one_attached :photos
+  has_many_attached :photos
   belongs_to :user
+
+  private
+
+  def image_type
+    if photos.attached? == true
+      photos.each do |photo|
+        print "image"
+        if !photo.content_type.in?(%('image/jpeg image/png'))
+          errors.add(:photos, 'needs to be a JPEG or PNG')
+        end
+      end
+    end
+  end
 end
