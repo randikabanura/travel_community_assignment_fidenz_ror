c09956d65ed8adfbf01d1f97d9bdd303d139a324
randikabanura <randika.banura@gmail.com>
2019-09-06 09:08:23 +0530

    Invariable error fixed in avatar uploading

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,18 @@
 module ApplicationHelper
 
-  def user_avatar(user, size=40)
+  def user_avatar(user, size = 40)
     if user.avatar.attached?
-      user.avatar.variant(resize: "#{size}x#{size}!")
+          if !user.avatar.content_type.in?(%('image/jpeg image/png'))
+            gravatar_url = "http://www.gravatar.com/avatar?s=#{size}"
+            image_url(gravatar_url, alt: user.name)
+          else
+            user.avatar.variant(resize: "#{size}x#{size}!");
+          end
     else
       gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
       gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
       image_url(gravatar_url, alt: user.name)
     end
   end
+
 end
