a89879e2d0c113ba8f6ed23a12a6197537f21d70
randikabanura <randika.banura@gmail.com>
2019-09-19 13:07:36 +0530

    Review submission issues fixed

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -6,17 +6,18 @@ module Api
 
       def create
         review = Review.new(review_params)
-        review.user= User.find(review.user_id)
+        review.user = User.find(review.user_id)
         review.trip = Trip.find(review.trip_id)
 
-        if Review.exists?(trip_id: review.trip_id)
-          @reviews = Review.all.where(trip: Trip.find(review.trip_id))
-        else
-          @reviews = nil
-        end
         if review.save
+          if Review.exists?(trip_id: review.trip_id)
+            @reviews = Review.all.where(trip: Trip.find(review.trip_id))
+          else
+            @reviews = nil
+          end
+
           respond_to do |format|
-            format.js {render :file => 'trips/review'}
+            format.js { render 'trips/review', locals: {reviews: @reviews} }
           end
         else
           render json: {status: 'SUCCESS', message: 'Review saved2'}, status: :internal_server_error
