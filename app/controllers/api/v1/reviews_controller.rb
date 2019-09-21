bdfd8b22982338e479b10ee7a5b099c18ab9364d
randikabanura <randika.banura@gmail.com>
2019-09-21 19:30:00 +0530

    Review edit function added

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -46,11 +46,42 @@ module Api
         end
       end
 
+      def edit
+        @review = Review.find(params[:id])
+        @trip = Trip.find(params[:trip_id])
+        respond_to do |format|
+          format.js { render 'trips/edit_review', locals: {review: @review, trip: @trip} }
+        end
+      end
+
+      def update
+        review = Review.find(update_review_params[:id])
+        if review.update(update_review_params)
+          if Review.exists?(trip_id: update_review_params[:trip_id])
+            @reviews = Review.all.where(trip: Trip.find(update_review_params[:trip_id]))
+          else
+            @reviews = nil
+          end
+          @trip = Trip.find(update_review_params[:trip_id])
+
+          respond_to do |format|
+            format.js { render 'trips/review', locals: {reviews: @reviews, trip: @trip} }
+          end
+        else
+          render json: {  error: review.errors.full_messages }, status: :bad_request
+        end
+      end
+
       private
 
       def review_params
         params.require(:review).permit(:user_id, :trip_id, :rating, :description)
       end
+
+      def update_review_params
+        params.require(:review).permit(:id, :user_id, :trip_id, :rating, :description)
+      end
+
     end
   end
 end
